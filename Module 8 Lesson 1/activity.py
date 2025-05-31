f375e896b0b839b1def6d40eed2ec3c3bb9a9451
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-05-31 15:35:08 -0700

    first commit

---
+++ Module 8 Lesson 1/activity.py
@@ -0,0 +1,18 @@
+name = "Obada Maryam"
+age = 17
+IsStudent = True
+PocketMoney = 100
+
+print("Name:" , name)
+print(type(name))
+print("Age:" , age)
+print(type(age))
+print(("Is Obada Maryam is Student:" , IsStudent))
+print(type(IsStudent))
+print("PocketMoney:" , PocketMoney)
+print(type(PocketMoney))
+
+PocketMoney = int(PocketMoney)
+print("The PocketMoney datatype is now" , type(PocketMoney))
+age = str(age)
+print("The datatype is now" , type(age))
