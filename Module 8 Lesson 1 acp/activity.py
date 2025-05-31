abbb66b195d73947af2719d4dfcc014c6deb6271
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-05-31 16:27:04 -0700

    Module 8 Lesson 1 acp

---
+++ Module 8 Lesson 1 acp/activity.py
@@ -0,0 +1,16 @@
+a = input("Enter a number for a:")
+b = input("Enter a number for b:")
+c = input("Enter a number for c:")
+
+print("The a value before swapping:" , a)
+print("The b value before swapping:" , b)
+print("The c value before swapping:" , c)
+
+temp = a
+a = b
+b = c
+c = temp
+
+print("The a value after swapping:" , a)
+print("The b value after swapping:" , b)
+print("The c value after swapping:" , c)
