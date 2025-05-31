7f18040ea845ef5754a8187cd5cf63885cd1ad53
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-05-31 15:47:53 -0700

    Activity2

---
+++ Module 8 Lesson 1/activity2.py
@@ -0,0 +1,12 @@
+m = input("Enter a number for m:")
+n = input("Enter a number for n:")
+
+print("The m value before swapping:" , m)
+print("The n value before swapping:" , n)
+
+temp = m
+m = n
+n = temp
+
+print("The m value after swapping:" , m)
+print("The n value after swapping:" , n)
