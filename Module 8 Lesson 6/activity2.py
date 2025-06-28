a37bf2b456c311f02cf2a14032892a7fbea657fd
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-06-28 16:11:47 -0700

    activity2

---
+++ Module 8 Lesson 6/activity2.py
@@ -0,0 +1,22 @@
+my_dict = {1 : "Maryam", 2 : 17, 3: "Student", 4: "Pakastani"}
+
+#printing values by using key name
+print(my_dict[1])
+
+#update
+my_dict[1] = "Obada"
+print(my_dict)
+
+my_dict[5] = "Bahawalpur"
+print(my_dict)
+
+#pop
+my_dict.pop(3)
+print(my_dict)
+
+#clear
+my_dict.clear()
+print(my_dict)
+
+
+
