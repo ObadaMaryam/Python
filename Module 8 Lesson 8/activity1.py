f52e8cc32b79934c23ee54abb886ce4db2b9883d
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-07-05 15:19:41 -0700

    activity1,2

---
+++ Module 8 Lesson 8/activity1.py
@@ -0,0 +1,10 @@
+class Student:
+    grade = 12
+    name = "Obada_Maryam"
+
+    def introduction(my):
+        print("Hi! I am", my.name)
+        print("I am a student of grade", my.grade)
+
+ob = Student()
+ob.introduction()
