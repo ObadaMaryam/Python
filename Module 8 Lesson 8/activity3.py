922c0730250a01af665f29653406de08906a8f85
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-07-05 15:49:28 -0700

    activity4

--- Module 8 Lesson 8/activity3.py
+++ Module 8 Lesson 8/activity3.py
@@ -1,9 +1,21 @@
 class Parrot:
 
+    #instance attributes
     def __init__(self, name,age):
          self.name = name
          self.age = age
 
+    #inttance method
+    def sing(self, song):
+         return "{} sings {}".format(self.name, song)
+    
+    #method
+    def dance(self):
+         return "{} is dancing.format(self.name)"
+
 obj = Parrot("Blu", 5)
 
 print("{} is {} years old".format(obj.name,obj.age))
+
+print(obj.sing("Happy"))
+print(obj.dance())
