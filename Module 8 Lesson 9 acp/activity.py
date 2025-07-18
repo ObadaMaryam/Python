8f64f50c9315a3ea8c238705c1cb538d929c9c97
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-07-18 16:56:42 -0700

    Module 8 Lesson 9 acp

---
+++ Module 8 Lesson 9 acp/activity.py
@@ -0,0 +1,13 @@
+class Expression:
+
+    def __init__(self,num1,num2,num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def add_num(self):
+        result = self.num1+self.num2+self.num3
+        print(result)
+
+add = Expression(5,10,15)
+add.add_num()
