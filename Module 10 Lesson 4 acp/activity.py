680721483c62ea47b22bfce016552a8b98c545ca
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-09-08 21:36:24 -0700

    Module 10 Lesson 4 acp

---
+++ Module 10 Lesson 4 acp/activity.py
@@ -0,0 +1,4 @@
+binary_number_str = input("Enter the binary number: ")
+decimal_equivalent = int(binary_number_str, 2)
+
+print(f"The decimal equivalent of binary {binary_number_str} is {decimal_equivalent}")
