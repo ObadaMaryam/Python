c1fdd7f6568db10876678fb9ad435430ab0e9d46
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-09-15 20:52:49 -0700

    activity1

---
+++ Module 11 Lesson 1/activity.py
@@ -0,0 +1,18 @@
+a = 19
+b = 15
+
+#And operator
+and_result = a & b
+print("The result of the AND Operator is ", and_result)
+
+#OR operator
+or_result = a | b
+print("The result of the OR Operator is ", or_result)
+
+#XOR operator
+xor_result = a ^ b
+print("The result of the XOR Operator is ", xor_result)
+
+#NOT operator
+not_result = ~a
+print("The result of the NOT Operator is ", not_result)
