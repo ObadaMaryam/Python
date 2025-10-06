60c35f1f709e9938b7012eb078c9c2f062a33f47
ObadaMaryam <ranaibrahimrajpoot47@gmail.com>
2025-10-05 17:20:48 -0700

    Module 11 Lesson 4 acp

---
+++ Module 11 Lesson 4 acp/activity.py
@@ -0,0 +1,19 @@
+def power8(num):
+    count = 0
+
+    if num & ~(num & (num-1)):
+        while num>1:
+            num = num >> 1
+            count = count + 1
+
+    if count % 2 == 0:
+        return True
+    else:
+        return False
+    
+num = int(input("Enter the Number: "))
+if power8(num):
+    print("The number is a power of 8")
+else:
+    print("The number is not a power of 8")
+    
